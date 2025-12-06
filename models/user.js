70033f63438b1fe73474e43bcbb4238dcabe3ff9
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-12-06 11:40:52 +0100

    Refactor user schema: remove unnecessary whitespace and ensure password hashing is handled correctly before saving

--- models/user.js
+++ models/user.js
@@ -8,7 +8,6 @@ const userSchema = new mongoose.Schema({
     minlength: 4,
     lowercase: true,
   },
-
   username: {
     type: String,
     required: true,
@@ -37,3 +36,16 @@ const userSchema = new mongoose.Schema({
     default: [],
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = mongoose.model(User, userSchema);
