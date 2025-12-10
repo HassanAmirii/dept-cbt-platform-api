85d7e911b7aa02fa504fef925fad80eb0f2f0cff
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-12-10 08:44:46 +0100

    fix: correct dotenv configuration and establish database connection; create app middleware

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-
-const app = express();
+const app = require("./middleware/app");
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("succesfully connected to DB");
+
+    app.listen(PORT, () => {
+      console.log(`app is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("database connection error", err);
+  });
